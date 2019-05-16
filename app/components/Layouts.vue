7377b2c05554004f21b2d916be01cb7e48f4ed2f
L16H7 <linmyatko@hotmail.com>
2019-05-16 13:18:31 +0630

    init

---
+++ app/components/Layouts.vue
@@ -0,0 +1,127 @@
+<template>
+  <Page>
+    <StackLayout>
+      <FlexBoxLayout
+        class="flex-box-layout border"
+      >
+        <StackLayout width="33%">
+          <Button
+          />
+        </StackLayout>
+        <StackLayout width="33%">
+          <Button
+          />
+        </StackLayout>
+        <StackLayout width="33%">
+          <Button
+          />
+        </StackLayout>
+      </FlexBoxLayout>  
+
+      <FlexBoxLayout
+        class="flex-box-layout border"
+      >
+        <StackLayout width="66%">`
+          <Button
+          />
+        </StackLayout>
+        <StackLayout width="33%">
+          <Button
+          />
+        </StackLayout>
+      </FlexBoxLayout>  
+
+      <GridLayout
+        columns="2*, *"
+        rows="auto"
+        class="border"
+      >
+        <StackLayout
+          row="0"
+          col="0"
+        >
+          <Button
+          />
+        </StackLayout>
+        <StackLayout
+          row="0"
+          col="1"
+        >
+          <Button
+          />
+        </StackLayout>
+      </GridLayout>
+
+      <FlexBoxLayout
+        class="flex-box-layout border"
+      >
+        <StackLayout width="25%">`
+          <Button
+          />
+        </StackLayout>
+        <StackLayout width="33%">
+          <Button
+          />
+        </StackLayout>
+        <StackLayout width="42%">
+          <Button
+          />
+        </StackLayout>
+      </FlexBoxLayout>  
+
+      <GridLayout
+        columns="3*, 4*, 5*"
+        rows="auto"
+        class="border"
+      >
+        <StackLayout
+          row="0"
+          col="0"
+        >
+          <Button
+          />
+        </StackLayout>
+        <StackLayout
+          row="0"
+          col="1"
+        >
+          <Button
+          />
+        </StackLayout>
+        <StackLayout
+          row="0"
+          col="2"
+        >
+          <Button
+          />
+        </StackLayout>
+      </GridLayout>
+
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+Button {
+  background-color: #00a5ff;
+  width: 70;
+}
+StackLayout {
+  border-width: 1;
+  border-color: red;
+  margin: 1;
+}
+.border {
+  border-width: 1;
+  border-color: black;
+  margin: 4;
+}
+.flex-box-layout {
+  justify-content: space-around;
+}
+</style>
