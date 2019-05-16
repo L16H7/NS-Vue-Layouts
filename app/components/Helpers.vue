fc9a10cc66dfd685a773fed1902afeac79eeb370
L16H7 <linmyatko@hotmail.com>
2019-05-16 16:38:22 +0630

    Helper component & Helper scss

---
+++ app/components/Helpers.vue
@@ -0,0 +1,128 @@
+<template>
+  <StackLayout>
+    <FlexboxLayout
+      justifyContent="space-around"
+      class="border"
+    >
+      <StackLayout
+        width="100%"
+        class="ma-1"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="ma-2"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="ma-3"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="ma-4"
+      >
+        <Button />
+      </StackLayout>
+    </FlexboxLayout>
+    <FlexboxLayout
+      justifyContent="space-around"
+      class="border"
+    >
+      <StackLayout
+        width="100%"
+        class="pa-1"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="pa-2"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="pa-3"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="pa-4"
+      >
+        <Button />
+      </StackLayout>
+    </FlexboxLayout>
+
+    <FlexboxLayout
+      justifyContent="space-around"
+      class="border"
+    >
+      <StackLayout
+        width="100%"
+        class="mt-2"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="mb-2"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="ml-2"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="mr-2"
+      >
+        <Button />
+      </StackLayout>
+    </FlexboxLayout>
+
+    <FlexboxLayout
+      justifyContent="space-around"
+      class="border"
+    >
+      <StackLayout
+        width="100%"
+        class="pt-2"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="pb-2"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="pl-2"
+      >
+        <Button />
+      </StackLayout>
+      <StackLayout
+        width="100%"
+        class="pr-2"
+      >
+        <Button />
+      </StackLayout>
+    </FlexboxLayout>
+
+  </StackLayout>
+</template>
+
+<script>
+export default {
+};
+</script>
